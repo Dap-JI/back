93995d411b2994c6d72e787c02eba4627ba38518
yhs0329 <scar0329@naver.com>
2024-07-25 17:47:04 +0900

    카카오 로그인 및 세션 생성 완료

--- models/gym.js
+++ models/gym.js
@@ -16,7 +16,7 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING(255),
         allowNull: false,
       },
-      img: {
+      logo: {
         type: DataTypes.STRING(255),
         allowNull: false,
       },
