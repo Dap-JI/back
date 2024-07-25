b3474d1bb772982765e023e9b327cfe5e24ad5d5
yhs0329 <scar0329@naver.com>
2024-07-25 15:34:31 +0900

    config 파일 env관리로 변경

--- models/index.js
+++ models/index.js
@@ -1,13 +1,14 @@
 const { Sequelize } = require("sequelize");
-const config = require("../config/config.json");
+const config = require("../config/config.js");
 
+const env = process.env.NODE_ENV || "development";
 const sequelize = new Sequelize(
-  config.development.database,
-  config.development.username,
-  config.development.password,
+  config[env].database,
+  config[env].username,
+  config[env].password,
   {
-    host: config.development.host,
-    dialect: config.development.dialect,
+    host: config[env].host,
+    dialect: config[env].dialect,
   }
 );
 
