63c68dddf5e74d976381dd72f0d5a46b00c55cee
yhs0329 <scar0329@naver.com>
2024-07-26 21:15:17 +0900

    네이버 로그인 추가

---
+++ controllers/CnaverAuth.js
@@ -0,0 +1,75 @@
+const axios = require("axios");
+const db = require("../models");
+const querystring = require("querystring");
+
+exports.naverLogin = (req, res) => {
+  const naverAuthURL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${process.env.NAVER_CLIENT_ID}&redirect_uri=${process.env.NAVER_REDIRECT_URI}&state=STATE_STRING`;
+  res.redirect(naverAuthURL);
+};
+
+exports.naverCallback = async (req, res) => {
+  const { code, state } = req.query;
+  try {
+    // Access token 요청
+    const tokenResponse = await axios({
+      method: "POST",
+      url: "https://nid.naver.com/oauth2.0/token",
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded",
+      },
+      data: querystring.stringify({
+        grant_type: "authorization_code",
+        client_id: process.env.NAVER_CLIENT_ID,
+        client_secret: process.env.NAVER_CLIENT_SECRET,
+        redirect_uri: process.env.NAVER_REDIRECT_URI,
+        code,
+        state,
+      }),
+    });
+
+    const { access_token } = tokenResponse.data;
+
+    // 사용자 정보 요청
+    const userResponse = await axios({
+      method: "GET",
+      url: "https://openapi.naver.com/v1/nid/me",
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+
+    const user = userResponse.data.response;
+    const provider = "naver";
+
+    // 사용자 정보 저장
+    const [userRecord, created] = await db.User.findOrCreate({
+      where: { email: user.email, provider: provider },
+      defaults: {
+        nickname: user.nickname,
+        email: user.email,
+        img: user.profile_image,
+        provider: user.provider,
+      },
+    });
+
+    if (!created) {
+      // 사용자 정보가 이미 존재하면 업데이트
+      await userRecord.update({
+        nickname: user.nickname,
+        img: user.profile_image,
+      });
+    }
+
+    // 사용자 정보 세션에 저장
+    req.session.user = {
+      nickname: user.nickname,
+      email: user.email,
+      img: user.profile_image,
+    };
+
+    res.json(req.session.user);
+  } catch (error) {
+    console.error("Error during Naver callback:", error);
+    res.status(500).send("Naver login failed");
+  }
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const express = require("express");
-const { kakaoLogin, kakaoCallback } = require("../controllers/Cauth");
+const { kakaoLogin, kakaoCallback } = require("../controllers/CkakaoAuth");
+const { naverLogin, naverCallback } = require("../controllers/CnaverAuth");
 const router = express.Router();
+
 /**
  * @swagger
  * /api/auth/kakao:
@@ -12,7 +14,6 @@ const router = express.Router();
  *         description: 카카오 로그인 페이지로 리디렉션
  */
 router.get("/kakao", kakaoLogin);
-
 /**
  * @swagger
  * /api/auth/kakao/callback:
@@ -46,6 +47,52 @@ router.get("/kakao", kakaoLogin);
  *         description: 서버 오류
  */
 router.get("/kakao/callback", kakaoCallback);
+
+/**
+ * @swagger
+ * /auth/naver:
+ *   get:
+ *     summary: 네이버 로그인
+ *     tags: [Auth]
+ *     responses:
+ *       302:
+ *         description: 네이버 로그인 페이지로 리디렉션
+ */
+router.get("/naver", naverLogin);
+
+/**
+ * @swagger
+ * /auth/naver/callback:
+ *   get:
+ *     summary: 네이버 로그인 콜백
+ *     tags: [Auth]
+ *     parameters:
+ *       - in: query
+ *         name: code
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: 네이버에서 받은 인증 코드
+ *     responses:
+ *       200:
+ *         description: 사용자 정보
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                 nickname:
+ *                   type: string
+ *                 email:
+ *                   type: string
+ *                 profile_image:
+ *                   type: string
+ *       500:
+ *         description: 서버 오류
+ */
+router.get("/naver/callback", naverCallback);
 
 /**
  * @swagger
