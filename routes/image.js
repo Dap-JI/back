7f7dac75542fb8ef122aa6accc06ed2e8f676f73
yhs0329 <scar0329@naver.com>
2024-07-29 16:03:36 +0900

    aws 폴더 구조 설정(클라이밍장 로고, 프로필 사진, 자유게시판)

--- routes/image.js
+++ routes/image.js
@@ -1,14 +1,18 @@
 const express = require("express");
-const upload = require("../middlewares/upload");
+const {
+  uploadGymLogo,
+  uploadProfilePicture,
+  uploadBoardImage,
+} = require("../middlewares/upload");
 const Cimage = require("../controllers/Cimage");
 
 const router = express.Router();
 
 /**
  * @swagger
- * /api/images:
+ * /api/images/gym-logo:
  *   post:
- *     summary: 이미지 업로드
+ *     summary: 클라이밍장 로고 이미지 업로드
  *     tags: [Images]
  *     consumes:
  *       - multipart/form-data
@@ -16,7 +20,7 @@ const router = express.Router();
  *       - in: formData
  *         name: image
  *         type: file
- *         description: The image to upload
+ *         description: 업로드할 로고 이미지
  *         required: true
  *     responses:
  *       200:
@@ -31,11 +35,88 @@ const router = express.Router();
  *                 imageUrl:
  *                   type: string
  *       400:
- *         description: No file uploaded
+ *         description: 파일이 업로드되지 않음
  *       500:
  *         description: 서버 오류
  */
+router.post(
+  "/images/gym-logo",
+  uploadGymLogo.single("image"),
+  imageController.uploadImage
+);
 
-router.post("/images", upload.single("image"), Cimage.uploadImage);
+/**
+ * @swagger
+ * /api/images/profile-picture:
+ *   post:
+ *     summary: 프로필 사진 업로드
+ *     tags: [Images]
+ *     consumes:
+ *       - multipart/form-data
+ *     parameters:
+ *       - in: formData
+ *         name: image
+ *         type: file
+ *         description: 업로드할 프로필 사진
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: 이미지 업로드 성공
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 imageUrl:
+ *                   type: string
+ *       400:
+ *         description: 파일이 업로드되지 않음
+ *       500:
+ *         description: 서버 오류
+ */
+router.post(
+  "/images/profile-picture",
+  uploadProfilePicture.single("image"),
+  imageController.uploadImage
+);
+
+/**
+ * @swagger
+ * /api/images/board-image:
+ *   post:
+ *     summary: 자유게시판 이미지 업로드
+ *     tags: [Images]
+ *     consumes:
+ *       - multipart/form-data
+ *     parameters:
+ *       - in: formData
+ *         name: image
+ *         type: file
+ *         description: 업로드할 게시판 이미지
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: 이미지 업로드 성공
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 imageUrl:
+ *                   type: string
+ *       400:
+ *         description: 파일이 업로드되지 않음
+ *       500:
+ *         description: 서버 오류
+ */
+router.post(
+  "/images/board-image",
+  uploadBoardImage.single("image"),
+  imageController.uploadImage
+);
 
 module.exports = router;
